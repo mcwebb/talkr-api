23d8fa85981a5b6338da2534f11326761e04ff3c
Matthew Webb <matthewowebb@gmail.com>
2016-07-18 12:08:53 +0530

    add user creation handling

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Joi = require('joi');
+const db = require('./lib/util/db');
 
-const server = new Hapi.Server();
+const server = new Hapi.Server({ debug: { request: ['error'] }});
 server.connection({ port: process.env.PORT || 3000 });
 
 server.route({
@@ -13,7 +15,32 @@ server.route({
   }
 });
 
-server.start((err) => {
+server.route({
+  method: 'POST',
+  path: '/api/user',
+  handler: require('./lib/users/handlers').create,
+  config: {
+    payload: {
+      output: 'data',
+      parse: true,
+      allow: 'application/json'
+    },
+    validate: {
+      payload: {
+        phone: Joi.object().keys({
+          cc: Joi.string().min(1).max(3),
+          number: Joi.string().min(4).max(16)
+        })
+      }
+    }
+  }
+});
+
+server.start(function (err) {
   if (err) throw err;
   console.log('Server running at:', server.info.uri);
+});
+
+server.on('stop', function () {
+  db.done();
 });
